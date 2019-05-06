2ca9a5d3722fd788998d174ea37bf308961bee43
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2019-05-06 09:16:53 -0600

    maxchar tests passing

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let map = {}
+  let count = 0
+  let maxC = ''
+
+  for (let char of str) {
+    if (!map[char]) {
+      map[char] = 1
+    }
+    else {
+      map[char]++
+    }
+  }
+
+  for (let max in map) {
+    if (map[max] > count) {
+      count = map[max]
+      maxC = max
+    }
+  }
+  return maxC
+}
 
 module.exports = maxChar;
